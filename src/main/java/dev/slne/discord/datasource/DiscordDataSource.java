744cfc9db56a09865187553d3ef8b9002a8d0e33
Simon <ammo@silence.ovh>
2023-09-05 00:28:20 +0200

    fix: Dont load pusher again

--- src/main/java/dev/slne/discord/datasource/DiscordDataSource.java
+++ src/main/java/dev/slne/discord/datasource/DiscordDataSource.java
@@ -2,12 +2,6 @@ package dev.slne.discord.datasource;
 
 import dev.slne.data.api.DataApi;
 import dev.slne.data.api.DataSource;
-import dev.slne.data.api.pusher.packet.PusherPacket;
-import dev.slne.data.core.pusher.CorePusherModule;
-import dev.slne.discord.DiscordBot;
-import dev.slne.discord.datasource.pusher.event.DiscordPusherEvent;
-
-import java.time.LocalDateTime;
 
 public class DiscordDataSource implements DataSource {
 
@@ -32,14 +26,6 @@ public class DiscordDataSource implements DataSource {
         dataInstance = new DiscordDataInstance();
         dataApi = new DataApi(dataInstance);
 
-        dataInstance.getDataModuleLoader().registerModule(new CorePusherModule() {
-            @Override
-            public void callPusherEvent(String channelName, String eventName, String userId,
-                                        PusherPacket pusherPacket) {
-                DiscordBot.getInstance().getListenerManager().broadcastEvent(new DiscordPusherEvent(pusherPacket,
-                        LocalDateTime.now(), channelName, eventName, userId, true));
-            }
-        });
         dataInstance.getDataModuleLoader().loadModules();
     }
 
