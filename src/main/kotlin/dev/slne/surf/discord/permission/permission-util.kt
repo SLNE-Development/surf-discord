133b3028bc9d8dfda755b2a9376a0c621b91c35b
TheBjoRedCraft <thebjoredcraft@gmail.com>
2025-10-31 17:03:20 +0100

    feat: add CastCrafter Discord Permissions

--- src/main/kotlin/dev/slne/surf/discord/permission/permission-util.kt
+++ src/main/kotlin/dev/slne/surf/discord/permission/permission-util.kt
@@ -4,7 +4,7 @@ import net.dv8tion.jda.api.entities.Member
 import net.dv8tion.jda.api.entities.User
 
 private val guildPermissionConfig: Map<Long, Map<Long, Set<DiscordPermission>>> = mapOf(
-    1410944184231137332 to mapOf(// Guild ID
+    1410944184231137332 to mapOf(// Arty Development Discord (Red)
         1432660473916035133 to setOf(*DiscordPermission.entries.toTypedArray()), // Admin
         1432660626643222528 to setOf(
             DiscordPermission.TICKET_WHITELIST_VIEW,
@@ -15,8 +15,82 @@ private val guildPermissionConfig: Map<Long, Map<Long, Set<DiscordPermission>>> 
             DiscordPermission.TICKET_CLAIM,
             DiscordPermission.COMMAND_TICKET_ADD,
             DiscordPermission.COMMAND_TICKET_REMOVE,
-            DiscordPermission.COMMAND_TICKET_WATCH
         ) // Supporter
+    ),
+    133198459531558912 to mapOf(// CastCrafter Discord
+        949704206888079490 to setOf(*DiscordPermission.entries.toTypedArray()), // Server Admin
+        156164562499010560 to setOf(
+            DiscordPermission.COMMAND_TICKET_ADD,
+            DiscordPermission.COMMAND_TICKET_REMOVE,
+            DiscordPermission.TICKET_CLOSE,
+            DiscordPermission.TICKET_CLAIM,
+            DiscordPermission.TICKET_WHITELIST_VIEW,
+            DiscordPermission.TICKET_WHITELIST_CONFIRM,
+            DiscordPermission.TICKET_SUPPORT_SURVIVAL_VIEW,
+            DiscordPermission.TICKET_SUPPORT_EVENT_VIEW,
+            DiscordPermission.TICKET_SUPPORT_DISCORD_VIEW,
+            DiscordPermission.TICKET_REPORT_VIEW,
+            DiscordPermission.TICKET_UNBAN_VIEW,
+            DiscordPermission.TICKET_BUG_VIEW,
+            DiscordPermission.TICKET_APPLICATION_VIEW
+        ), // Discord Moderation
+        1350468732555755570L to setOf( // Management
+            DiscordPermission.COMMAND_TICKET_ADD,
+            DiscordPermission.COMMAND_TICKET_REMOVE,
+            DiscordPermission.TICKET_CLOSE,
+            DiscordPermission.TICKET_CLAIM,
+            DiscordPermission.TICKET_WHITELIST_VIEW,
+            DiscordPermission.TICKET_WHITELIST_CONFIRM,
+            DiscordPermission.TICKET_SUPPORT_SURVIVAL_VIEW,
+            DiscordPermission.TICKET_SUPPORT_EVENT_VIEW,
+            DiscordPermission.TICKET_REPORT_VIEW,
+            DiscordPermission.TICKET_UNBAN_VIEW,
+            DiscordPermission.TICKET_APPLICATION_VIEW
+        ),
+
+        1242929429747994664L to setOf( // Developer
+            DiscordPermission.COMMAND_TICKET_ADD,
+            DiscordPermission.COMMAND_TICKET_REMOVE,
+            DiscordPermission.TICKET_CLOSE,
+            DiscordPermission.TICKET_CLAIM,
+            DiscordPermission.TICKET_WHITELIST_VIEW,
+            DiscordPermission.TICKET_WHITELIST_CONFIRM,
+            DiscordPermission.TICKET_SUPPORT_SURVIVAL_VIEW,
+            DiscordPermission.TICKET_SUPPORT_EVENT_VIEW,
+            DiscordPermission.TICKET_REPORT_VIEW,
+            DiscordPermission.TICKET_UNBAN_VIEW,
+            DiscordPermission.TICKET_BUG_VIEW
+        ),
+
+        1242929223593758811L to setOf( // Moderator
+            DiscordPermission.COMMAND_TICKET_ADD,
+            DiscordPermission.COMMAND_TICKET_REMOVE,
+            DiscordPermission.TICKET_CLOSE,
+            DiscordPermission.TICKET_CLAIM,
+            DiscordPermission.TICKET_WHITELIST_VIEW,
+            DiscordPermission.TICKET_WHITELIST_CONFIRM,
+            DiscordPermission.TICKET_SUPPORT_SURVIVAL_VIEW,
+            DiscordPermission.TICKET_SUPPORT_EVENT_VIEW,
+            DiscordPermission.TICKET_REPORT_VIEW,
+            DiscordPermission.TICKET_UNBAN_VIEW
+        ),
+
+        1242929497041277099L to setOf( // Supporter
+            DiscordPermission.COMMAND_TICKET_ADD,
+            DiscordPermission.COMMAND_TICKET_REMOVE,
+            DiscordPermission.TICKET_CLOSE,
+            DiscordPermission.TICKET_CLAIM,
+            DiscordPermission.TICKET_WHITELIST_VIEW,
+            DiscordPermission.TICKET_WHITELIST_CONFIRM,
+            DiscordPermission.TICKET_SUPPORT_SURVIVAL_VIEW,
+            DiscordPermission.TICKET_SUPPORT_EVENT_VIEW,
+            DiscordPermission.TICKET_REPORT_VIEW,
+            DiscordPermission.TICKET_UNBAN_VIEW
+        ),
+
+        1403107386415386736L to setOf(), // Community Management
+        1242929846481453107L to setOf(), // Builder
+        1001166778287792158L to setOf() // Server Team
     )
 )
 
