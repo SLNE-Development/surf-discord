68b26dba193a3412ea8d852239968035c229f606
TheBjoRedCraft <thebjoredcraft@gmail.com>
2026-02-03 16:11:25 +0100

    refactor: use `char` instead of `varchar` for `claimed_by` column in `TicketStaffTable`
    - Updated database schema to use `char` for better data consistency and fixed-length storage.

--- src/main/kotlin/dev/slne/surf/discord/ticket/database/ticket/staff/TicketStaffTable.kt
+++ src/main/kotlin/dev/slne/surf/discord/ticket/database/ticket/staff/TicketStaffTable.kt
@@ -8,7 +8,7 @@ object TicketStaffTable : LongIdTable("ticket_staff") {
     val ticketId = ulong("ticket_id").references(TicketTable.id)
     val claimedAt = zonedDateTime("claimed_at").nullable()
     val claimedBy =
-        varchar("claimed_by", 20).transform({ it.toLong() }, { it.toString() }).nullable()
+        char("claimed_by", 20).transform({ it.toLong() }, { it.toString() }).nullable()
     val claimedByName = varchar("claimed_by_name", 100).nullable()
     val claimedByAvatar = varchar("claimed_by_avatar", 200).nullable()
 }
