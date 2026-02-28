946fc4f3956db4debe02dae5de8ac552c2971f51
TheBjoRedCraft <thebjoredcraft@gmail.com>
2026-02-28 17:58:54 +0100

    refactor: remove permission check from WhitelistCreateButton

--- src/main/kotlin/dev/slne/surf/discord/interaction/button/impl/WhitelistCreateButton.kt
+++ src/main/kotlin/dev/slne/surf/discord/interaction/button/impl/WhitelistCreateButton.kt
@@ -1,14 +1,10 @@
 package dev.slne.surf.discord.interaction.button.impl
 
-import dev.slne.surf.discord.dsl.embed
 import dev.slne.surf.discord.getBean
 import dev.slne.surf.discord.interaction.button.DiscordButton
 import dev.slne.surf.discord.interaction.modal.ModalRegistry
 import dev.slne.surf.discord.messages.translatable
-import dev.slne.surf.discord.permission.DiscordPermission
-import dev.slne.surf.discord.permission.hasPermission
 import dev.slne.surf.discord.ticket.database.whitelist.SocialService
-import dev.slne.surf.discord.util.Colors
 import dev.slne.surf.discord.util.Emojis
 import net.dv8tion.jda.api.components.buttons.Button
 import net.dv8tion.jda.api.components.buttons.ButtonStyle
@@ -35,16 +31,6 @@ class WhitelistCreateButton(
     }
 
     override suspend fun onClick(event: ButtonInteractionEvent) {
-        if (!event.member.hasPermission(DiscordPermission.WHITELIST_BYPASS)) {
-            event.replyEmbeds(embed {
-                title = "Fehlgeschlagen"
-                description =
-                    "Zurzeit können keine Whitelist Anträge eingereicht werden. Der Survival Server ist zurzeit in Wartungsarbeiten und somit eine Whitelist nicht möglich. Bitte habe Verständnis."
-                color = Colors.WARNING
-            }).setEphemeral(true).queue()
-            return
-        }
-
         if (socialService.isWhitelisted(event.user.idLong)) {
             event.reply(translatable("whitelist.survival.modal.already_whitelisted"))
                 .setEphemeral(true)
