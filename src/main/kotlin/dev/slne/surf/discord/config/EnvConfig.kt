eedb2795cfb4ea9665d8774c1458062471f3cd05
TheBjoRedCraft <thebjoredcraft@gmail.com>
2026-07-22 12:55:04 +0200

    feat: refactor configuration to use environment variables for bot settings

---
+++ src/main/kotlin/dev/slne/surf/discord/config/EnvConfig.kt
@@ -0,0 +1,42 @@
+package dev.slne.surf.discord.config
+
+object EnvConfig {
+    private val env by lazy {
+        System.getenv()
+    }
+
+    val BOT_TOKEN get() = env.require("BOT_TOKEN")
+
+    val TICKET_CHANNEL get() = env.requireLong("TICKET_CHANNEL")
+    val TICKET_LOG_CHANNEL get() = env.requireLong("TICKET_LOG_CHANNEL")
+
+    val DB_HOSTNAME get() = env.require("DB_HOSTNAME")
+    val DB_PORT get() = env.requireInt("DB_PORT")
+    val DB_USERNAME get() = env.require("DB_USERNAME")
+    val DB_PASSWORD get() = env.require("DB_PASSWORD")
+    val DB_NAME get() = env.require("DB_NAME")
+
+    val LUCKPERMS_URL get() = env.optional("LUCKPERMS_URL")
+    val LUCKPERMS_TOKEN get() = env.optional("LUCKPERMS_TOKEN")
+
+    val WHITELIST_ROLE_ID get() = env.requireLong("WHITELIST_ROLE_ID")
+    val PREMIUM_ROLE_ID get() = env.optional("PREMIUM_ROLE_ID")
+
+    private fun Map<String, String>.optional(name: String) =
+        this[name]?.trim()?.takeIf(String::isNotEmpty)
+
+    private fun Map<String, String>.require(name: String) =
+        optional(name) ?: error("Missing required environment variable: $name")
+
+    private fun Map<String, String>.optionalInt(name: String) = optional(name)?.toIntOrNull()
+        ?: optional(name)?.let { error("Environment variable $name must be an integer") }
+
+    private fun Map<String, String>.optionalLong(name: String) = optional(name)?.toLongOrNull()
+        ?: optional(name)?.let { error("Environment variable $name must be an integer") }
+
+    private fun Map<String, String>.requireLong(name: String) =
+        optionalLong(name) ?: error("Missing required environment variable: $name")
+
+    private fun Map<String, String>.requireInt(name: String) =
+        optionalInt(name) ?: error("Missing required environment variable: $name")
+}
--- src/main/kotlin/dev/slne/surf/discord/config/RoleConfig.kt
+++ src/main/kotlin/dev/slne/surf/discord/config/RoleConfig.kt
@@ -3,4 +3,4 @@ package dev.slne.surf.discord.config
 import kotlinx.serialization.Serializable
 
 @Serializable
-data class RoleConfig(val premiumRoleId: String = "")
+data class RoleConfig(val premiumRoleId: String? = "")
